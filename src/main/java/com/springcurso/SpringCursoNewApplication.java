e2b87a1cba4a5f135663cedd6ef3fdcf14a7a176
Ivan <ivanfernando@gmail.com>
2019-06-15 20:41:09 -0300

    Initial commit

---
+++ src/main/java/com/springcurso/SpringCursoNewApplication.java
@@ -0,0 +1,13 @@
+package com.springcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCursoNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCursoNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcurso/SpringCursoNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcurso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCursoNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
